afd087e3e2326d34eabba6114de401df60e9e1b4
Brent Edwards <chipuni@comcast.net>
2021-10-23 18:26:25 -0700

    Adding more to convert_word_to_ipa.

--- src/text_to_ipa.py
+++ src/text_to_ipa.py
@@ -1,21 +1,38 @@
 """Convert English text to (more or less) IPA
 This is only a rough conversion, because that's all I need for shorthand."""
 
+# To do:
+# 1. Handle suffixes that double their letters, for example getting -> get
+# 2. Handle apostrophes in the middle of a word.
+
+# Not needed for this project:
+# 1. Final "s" pronunciation depends on whether the previous letter was voiced or unvoiced.
+
 import csv
-from nltk.stem import WordNetLemmatizer
 import re
 import sys
+
+from nltk.stem import WordNetLemmatizer
 
 
 def main(filename):
     """The main routine, which calls everything else."""
-    word_to_ipa = load_dictionary("../data/word_to_ipa.csv")
+    word_to_ipa = load_dictionary("data/word_to_ipa.csv")
+    suffix_to_ipa = load_dictionary("data/suffix_to_ipa.csv")
     with open(filename) as file:
         text = file.read()
-    split_word = list(filter(lambda word: (len(word) > 0 and word.isalpha()), split_by_word(text)))
-    split_ipa = list(zip(*[convert_word_to_ipa(word.lower(), word_to_ipa) for word in split_word]))
-    split_ipa[0] = split_ipa[0]
-    split_ipa[1] = split_ipa[1]
+
+    split_word = list(
+        filter(lambda word: (len(word) > 0 and word.isalpha()), split_by_word(text))
+    )
+    split_ipa = list(
+        zip(
+            *[
+                convert_word_to_ipa(word.lower(), word_to_ipa, suffix_to_ipa)
+                for word in split_word
+            ]
+        )
+    )
     return split_ipa
 
 
@@ -39,30 +56,44 @@ def split_by_word(text):
 lemmatizer = WordNetLemmatizer()
 
 
-def convert_word_to_ipa(word, word_to_ipa):
+def convert_word_to_ipa(word, word_to_ipa, suffix_to_ipa):
     """Convert an individual word to its IPA equivalent."""
     if word in word_to_ipa:
-        return word_to_ipa[word], ""
+        return word_to_ipa[word], "", ""
 
-    # TODO:
-    # 1. Find the word ending that's lost from lemmatization.
-    # 2. Convert those word endings to IPA.
-    # 3. Add them.
+    for pos in ["n", "v", "a", "r", "s"]:
+        lemma = lemmatizer.lemmatize(word, pos=pos)
+        if lemma in word_to_ipa:
+            print(f"Found by lemmatizing (type = {pos}): {word} -> {lemma}")
 
-    for pos in ['n', 'v', 'a', 'r', 's']:
-        stem = lemmatizer.lemmatize(word, pos=pos)
-        if stem in word_to_ipa:
-            print(f"Found by stemming (type = {pos}): {word} -> {stem}")
-            return word_to_ipa[stem], ""
+            # Find how long there are letters in common; everything after is the suffix.
+            for triple in zip(word, lemma, range(len(word))):
+                first_not_matching = triple[2]
+                if triple[0] != triple[1]:
+                    break
+
+            if word[first_not_matching] == lemma[first_not_matching]:
+                first_not_matching += 1
+
+            suffix = word[first_not_matching:]
+
+            if suffix in suffix_to_ipa:
+                return word_to_ipa[lemma] + suffix_to_ipa[suffix], "", ""
+
+            return "", "", f"{suffix} ({word}, {lemma})"
 
     print(f"NOT found by stemming: {word}")
 
-    return "", word
+    return "", word, ""
 
 
 if __name__ == "__main__":
     result = main(sys.argv[1])
-    unknown_words = sorted(list(set(result[1])))
 
     print("The words not in the dictionary are:")
+    unknown_words = sorted(list(set(result[1])))
     print(unknown_words)
+
+    print("The suffixes not in the dictionary are:")
+    unknown_suffixes = sorted(list(set(result[2])))
+    print(unknown_suffixes)
